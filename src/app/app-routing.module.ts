9fed307fe5f1e3d7f8e54197daa28876d382972c
shapirab <shapirab@jamd.ac.il>
2024-01-22 08:55:55 +0200

    22/1/24 fixed the issue with router outlet so that home component now works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { DepartmentsComponent } from './departments/departments.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {
+    path: 'home',
+    children: [
+      { path: 'employees', component: EmployeesComponent },
+      { path: 'departments', component: DepartmentsComponent },
+      { path: '', redirectTo: 'employees', pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { ContentComponent } from './content/content.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EmployeesComponent,
+    DepartmentsComponent,
+    ContentComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
